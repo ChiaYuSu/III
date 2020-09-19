b3b40951b29d9c3c9ab1c3c495152d5e299f73d2
ChiaYuSu <jack879tw@gmail.com>
2020-09-19 16:26:58 +0800

    rename

--- plot.py
+++ plot.py
@@ -9,7 +9,7 @@ import pprint
 import datetime as dt
 import matplotlib.dates as mdates
 
-num = "4167"
+num = "4051"
 case = "Case " + num
 
 # For SSL certificate
@@ -25,7 +25,7 @@ with request.urlopen(src) as response:
 #     json.dump(data, f, ensure_ascii=False, indent=4)
     
 # TFC timestamp
-tfc_timestamp = int(str(1594872000)) #
+tfc_timestamp = int(str(1593057600)) #
 
 # Get the smallest timestamp
 lists = []
